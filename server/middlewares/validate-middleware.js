d7c25ce9be0abdd52e8a897ea60cfb1835deb553
Rishikesh_Dongre <rishidongre519@gmail.com>
2024-01-25 17:22:15 +0530

    error handling

--- server/middlewares/validate-middleware.js
+++ server/middlewares/validate-middleware.js
@@ -5,11 +5,13 @@ const validate = (schema) => async (req, res, next) => {
     next();
   } catch (err) {
     const status = 422;
-    const message = err.errors[0].message;
+    const message = "Fill the input properly"
+    const extraDetails = err.errors[0].message;
 
     const error = {
       status,
       message,
+      extraDetails
     };
     console.log(error);
     // res.status(400).json({ msg: message });
