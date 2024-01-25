34888f23e4c0d76380038a16d01af446344032b9
Rishikesh_Dongre <rishidongre519@gmail.com>
2024-01-25 18:31:43 +0530

    contact form Schema

--- server/server.js
+++ server/server.js
@@ -3,14 +3,15 @@ const express = require("express");
 
 const app = express();
 
-const router = require("./router/auth-router");
-
+const authRoute = require("./router/auth-router");
+const contactRoute = require('./router/contact-router')
 const connectDb = require("./utils/db");
 const errorMiddleware = require("./middlewares/error-middleware");
 
 app.use(express.json());
 
-app.use("/api/auth", router);
+app.use("/api/auth", authRoute);
+app.use("/api/form", contactRoute);
 
 app.use(errorMiddleware);
 
