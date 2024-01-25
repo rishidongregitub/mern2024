b84b052f59af11b8cfb6dd72e5bb229c2e50e90d
Rishikesh_Dongre <rishidongre519@gmail.com>
2024-01-25 18:58:22 +0530

    This is frontend code

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import Service from './Pages/Service';
+import Register from './Pages/Register';
+import Login from './Pages/Login';
+
+
+const App = () => {
+  return (
+    <>
+        <BrowserRouter>
+          <Routes>
+            <Route  path='/' element={<Home/>}/>
+            <Route  path='/about' element={<About/>}/>
+            <Route  path='/contact' element={<Contact/>}/>
+            <Route  path='/service' element={<Service/>}/>
+            <Route  path='/register' element={<Register/>}/>
+            <Route  path='/login' element={<Login/>}/>
+          </Routes>
+        </BrowserRouter>
+    </>
+  );
+};
+export default App;
